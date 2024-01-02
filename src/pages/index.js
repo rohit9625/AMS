d11cd82d2d0805b3e9a49bc3fc51f899daa6b7ee
Rohit Verma <rv17837@outlook.com>
2024-01-02 14:54:10 +0530

    first deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,174 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Head from "next/head";
+import Script from "next/script";
+import Image from "next/image";
+import Link from "next/link";
+import NavBar from "../components/NavBar";
+import Dashboard from "@/components/Dashboard";
+import Credits from "@/components/Credits";
+import { isTokenValid } from "@/utils/auth";
+import { useState, useEffect } from "react";
+import Attendance from "@/components/Attendance";
 
-const inter = Inter({ subsets: ['latin'] })
+// This function will run on the server
+// This function will run on the server
+export async function getServerSideProps(context) {
+  const token = context.req.cookies.token || ""; // Retrieve the token from cookies
 
-export default function Home() {
+  const isLoggedIn = isTokenValid(token);
+  return {
+    props: {
+      isLoggedIn,
+    },
+  };
+}
+
+export default function Home({ isLoggedIn }) {
+  const [activeSection, setActiveSection] = useState("dashboard");
+  const [students, setStudents] = useState([]);
+  const [enrollmentOptions, setEnrollmentOptions] = useState([]);
+  const [subjects, setSubjects] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Fetch students
+        const studentsResponse = await fetch("/api/students");
+        const studentsData = await studentsResponse.json();
+        setStudents(studentsData);
+
+        // Fetch enrollmentOptions
+        const enrollmentResponse = await fetch("/api/enrollment");
+        const enrollmentOptionsData = await enrollmentResponse.json();
+        setEnrollmentOptions(enrollmentOptionsData);
+
+        // Fetch subjects
+        const subjectsResponse = await fetch("/api/subjects");
+        const subjectsData = await subjectsResponse.json();
+        setSubjects(subjectsData.subjects);
+
+        // Cache the data in localStorage
+        localStorage.setItem("students", JSON.stringify(studentsData));
+        localStorage.setItem(
+          "enrollmentOptions",
+          JSON.stringify(enrollmentOptionsData)
+        );
+        localStorage.setItem("subjects", JSON.stringify(subjectsData.subjects));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    // Check if data is already in cache
+    const cachedStudents = localStorage.getItem("students");
+    const cachedEnrollmentOptions = localStorage.getItem("enrollmentOptions");
+    const cachedSubjects = localStorage.getItem("subjects");
+
+    if (!cachedStudents || !cachedEnrollmentOptions || !cachedSubjects) {
+      // Data not in cache, fetch it
+      fetchData();
+    } else {
+      // Data is in cache, use it
+      setStudents(JSON.parse(cachedStudents));
+      setEnrollmentOptions(JSON.parse(cachedEnrollmentOptions));
+      setSubjects(JSON.parse(cachedSubjects));
+    }
+  }, []);
+
+  const handleLogout = () => {
+    // Remove data from localStorage
+    localStorage.removeItem("students");
+    localStorage.removeItem("enrollmentOptions");
+    localStorage.removeItem("subjects");
+
+    document.cookie =
+      "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=localhost;";
+
+    // Reload the page
+    window.location.reload();
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <Head>
+        <title>AMS : Attendance Management System</title>
+        <meta name="description" content="" />
+      </Head>
+      <Script
+        type="module"
+        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+      />
+      <Script
+        strategy="beforeInteractive"
+        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
+      />
+      {!isLoggedIn ? (
+        <div className="w-full h-screen bg-slate-300 flex justify-center items-center">
+          <div className="w-full max-w-[460px] h-2/3 bg-white mx-4 rounded-lg shadow-xl shadow-slate-400 flex space-y-14 flex-col items-center">
+            <div>
+              <h1 className="text-center text-3xl font-semibold pt-4">
+                Welcom to AMS
+              </h1>
+              <p className="text-sm">Your Attendance Management System</p>
+            </div>
+            {/* Actions */}
+            <div className="text-md w-1/2 space-y-4 text-center p-2">
+              <p className="text-md">Continue as</p>
+              <div className="w-full py-2 rounded-lg border font-semibold hover:bg-cyan-700 hover:text-white hover:shadow-lg shadow-cyan-300 transition-all duration-300 cursor-pointer text-slate-600 border-cyan-700">
+                <Link className="" href="/login">
+                  Admin
+                </Link>
+              </div>
+              <div className="w-full py-2 rounded-lg border font-semibold hover:bg-cyan-700 hover:text-white hover:shadow-lg shadow-cyan-300 transition-all duration-300 cursor-pointer text-slate-600 border-cyan-700">
+                <Link className="w-full" href="#">
+                  Faculty
+                </Link>
+              </div>
+              <div className="w-full py-2 rounded-lg border font-semibold hover:bg-cyan-700 hover:text-white hover:shadow-lg shadow-cyan-300 transition-all duration-300 cursor-pointer text-slate-600 border-cyan-700">
+                <Link className="w-full" href="#">
+                  Student
+                </Link>
+              </div>
+            </div>
+
+            {/* Credits */}
+            <Credits />
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      ) : (
+        <div className="flex">
+          {/* Left Side Bar */}
+          <NavBar
+            setActiveSection={setActiveSection}
+            activeSection={activeSection}
+          />
+          {/* Right Side Bar */}
+          <div className="w-full h-screen flex flex-col">
+            {/* Header */}
+            <div className="bg-white w-full h-20 relative flex justify-center items-end p-4">
+              <button
+                onClick={handleLogout}
+                className="text-4xl absolute right-8 top-4 cursor-pointer rounded-lg transition-all duration-300 hover:shadow-2xl shadow-slate-700"
+              >
+                <ion-icon name="log-out-outline"></ion-icon>
+              </button>
+              <span className="bg-white w-1/2 h-10 rounded-full text-center border-2">
+                Search Bar
+              </span>
+            </div>
+            {/* Main Div */}
+            {activeSection === "dashboard" ? (
+              <Dashboard
+                students={students}
+                enrollmentOptions={enrollmentOptions}
+              />
+            ) : activeSection === "attendance" ? (
+              <Attendance students={students} subjects={subjects} />
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
